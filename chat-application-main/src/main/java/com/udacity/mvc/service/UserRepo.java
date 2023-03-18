54bbf07a3bff418ca2cc1879cb697cd884f2bda3
Smartplus50 <102454391+Smartplus50@users.noreply.github.com>
2023-03-18 21:51:06 +0800

    Delete chat-application-main directory

--- chat-application-main/src/main/java/com/udacity/mvc/service/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.udacity.mvc.service;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.udacity.mvc.model.User;
-
-public interface UserRepo extends CrudRepository <User, Long> {
-    
-}
--- chat-application-main/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO USERS (userid, username, salt, password, firstname, lastname) VALUES
-(2, 'anas', 'anas', 'anas', 'anas', 'anas');
-
-INSERT INTO TUTORS (tutorid, tutorname, tutorpassword, firsttutorname, lasttutorname) VALUES
-(2, 'Brijesh', 'Brijesh', 'Brijesh', 'Brijesh');
